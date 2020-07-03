9b2164778fe05974fcac02857279e9fa4edd98ae
WilliamSeele <tangyinglin0228@gmail.com>
2020-07-03 16:44:31 +0800

    Experiment

--- TestVCR/ContentView.swift
+++ TestVCR/ContentView.swift
@@ -11,26 +11,43 @@ import GoogleMobileAds
 
 struct ContentView: View {
     @State var showInterstitial = true
+    @State var popSheet = false
     @EnvironmentObject var sharing:SharedValues
     var body: some View {
-        ZStack{
+        VStack{
 //            if showInterstitial{
 //                InterstitialVC()
 //                   .edgesIgnoringSafeArea(.all)
 //            }
-            NavigationView{
-                List{
-                    NavigationLink("Show Second View", destination: SecondView())
-                }
+
+            Button(action: {
+                self.popSheet = true
+            }) {
+                Text("Pop Sheet Second View")
             }
+            .sheet(isPresented: self.$popSheet) {
+                SecondView()
+                    .environmentObject(self.sharing)
+            }
+            NavigationView{
+                 NavigationLink("Show Second View", destination: SecondView())
+            }
+           
+                
+            
            
             
             
         }.onAppear{
             
-            let request = GADRequest()
-            self.sharing.interstitial.load(request)
+            
+            self.sharing.interstitial.load(self.sharing.req)
         }
+//        .onAppear{
+//            
+//            self.sharing.rewardedAdUnit.load(self.sharing.rewaredAdRequest)
+//            
+//        }
         
 
     }
@@ -80,8 +97,8 @@ struct InterstitialVC:UIViewControllerRepresentable{
         
         func interstitialDidDismissScreen(_ ad: GADInterstitial) {
             //do your things
-            let request = GADRequest()
-            parent.sharing.interstitial.load(request)
+            let req = GADRequest()
+            parent.sharing.interstitial.load(req)
         }
         
         func interstitialWillPresentScreen(_ ad: GADInterstitial) {
@@ -102,4 +119,182 @@ struct InterstitialVC:UIViewControllerRepresentable{
         }
     }
     
+}
+
+
+
+struct RewardedVC:UIViewControllerRepresentable{
+   
+    @EnvironmentObject var sharing:SharedValues
+    
+    var counterRewardedAd = 0
+    
+    
+    func makeCoordinator() -> Coordinator {
+        
+        Coordinator(self)
+    }
+    
+    class Coordinator:NSObject, GADRewardedAdDelegate{
+        
+        
+        var parent:RewardedVC!
+        
+        init(_ parent: RewardedVC) {
+            
+            self.parent = parent
+        }
+        
+        func rewardedAd(_ rewardedAd: GADRewardedAd, userDidEarn reward: GADAdReward) {
+            
+            print("Unlocking feature")
+                   
+                 
+       NotificationCenter.default.post(name: NSNotification.Name("channelUnlockMonthlyChart"), object: nil)
+                        
+        }
+        
+        func rewardedAdDidDismiss(_ rewardedAd: GADRewardedAd) {
+            print("debugging rewarded ad rewardedAdDidDismiss \(String(describing: rewardedAd))")
+            parent.loadRewarded()
+        }
+        
+        func rewardedAdDidPresent(_ rewardedAd: GADRewardedAd) {
+             print("debugging rewarded ad rewardedAdDidPresent \(String(describing: rewardedAd))")
+            parent.sharing.showingUniqueAd = true
+        }
+        
+        func rewardedAd(_ rewardedAd: GADRewardedAd, didFailToPresentWithError error: Error) {
+            print("debugging rewarded ad rewardedAdDidPresent \(String(describing: error.localizedDescription))")
+            parent.sharing.showingUniqueAd = false
+            parent.loadRewarded()
+        }
+        
+        
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        print("sending rewards? \(uiViewController)")
+        if sharing.showingUniqueAd == false{
+//
+//            if sharing.rewardedAdUnit.isReady {
+//                sharing.showingUniqueAd = true
+                showAD(controller: uiViewController, delegate: context.coordinator){
+
+                    print("sending rewards? \(uiViewController)")
+
+                }
+
+//            }else{
+//                print("rewarded ad ready \(sharing.rewardedAdUnit)")
+//            }
+        }
+        
+    }
+    
+    
+    func makeUIViewController(context: Context) -> UIViewController{
+
+        for vc in UIApplication.shared.windows{
+            
+            print("debugging views \(String(describing: vc.rootViewController?.classForCoder))")
+            
+        }
+//        let viewController = UIViewController()
+        let viewController = UIApplication.shared.windows.first?.rootViewController
+        
+//        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+        viewController!.view.frame = UIScreen.main.bounds
+        
+//        }
+//        loadRewarded()
+
+        return viewController ?? UIViewController()
+    }
+    
+        func loadRewarded(){
+            guard counterRewardedAd == 0 else {
+                return
+            }
+            sharing.rewardedAdUnit.load(sharing.rewardedAdRequest)
+        
+        }
+        
+    func showAD(controller:UIViewController, delegate:Coordinator, rewardFunction: @escaping() -> Void){
+            guard counterRewardedAd == 0 else {
+                return
+            }
+            
+    //        let root = UIApplication.shared.windows.first?.rootViewController
+             
+            if sharing.rewardedAdUnit.isReady{
+                self.sharing.rewardFunction = rewardFunction
+    //            let root = UIApplication.shared.windows.first?.rootViewController
+    //            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+1) {
+    //                 let root = controller
+               sharing.showingUniqueAd = true
+                sharing.rewardedAdUnit.present(fromRootViewController:controller , delegate: delegate)
+    //            }
+                
+            }
+
+            }
+    
+}
+
+
+final class Rewarded: NSObject, GADRewardedAdDelegate{
+   
+    var rewardFunction: (() -> Void)? = nil
+    
+    var rewardedAd:GADRewardedAd!
+    
+    init(ad:GADRewardedAd!) {
+        super.init()
+        rewardedAd = ad
+        LoadRewarded()
+    }
+    
+    func LoadRewarded(){
+        let req = GADRequest()
+        self.rewardedAd.load(req)
+    }
+    
+    func showAd(rewardFunction: @escaping () -> Void){
+        
+        if self.rewardedAd.isReady{
+            self.rewardFunction = rewardFunction
+            for vc in UIApplication.shared.windows{
+                
+                print("debugging views \(String(describing: vc))")
+                
+            }
+            print("debugging views \(UIApplication.shared.windows.count)")
+            let root = UIApplication.shared.windows.last?.rootViewController
+//            let root = UIViewController()
+            self.rewardedAd.present(fromRootViewController: root!, delegate: self)
+            print("no vc root \(String(describing: root))")
+        }
+       else{
+           print("Not Ready")
+       }
+    }
+    
+    func rewardedAd(_ rewardedAd: GADRewardedAd, userDidEarn reward: GADAdReward) {
+        if let rf = rewardFunction {
+            rf()
+        }
+    }
+    func rewardedAdDidPresent(_ rewardedAd: GADRewardedAd) {
+        print("debugging error rewardedAdDidPresent \(rewardedAd)")
+    }
+    func rewardedAdDidDismiss(_ rewardedAd: GADRewardedAd) {
+        self.rewardedAd = GADRewardedAd(adUnitID: "ca-app-pub-3940256099942544/1712485313")
+        LoadRewarded()
+    }
+    
+    func rewardedAd(_ rewardedAd: GADRewardedAd, didFailToPresentWithError error: Error) {
+        print("debugging error didFailToPresentWithError \(String(describing: rewardedAd))")
+        print("debugging error didFailToPresentWithError \(error.localizedDescription)")
+    }
 }
--- TestVCR/SharedValues.swift
+++ TestVCR/SharedValues.swift
@@ -10,6 +10,11 @@ import Foundation
 import GoogleMobileAds
 class SharedValues:ObservableObject{
     
-    @Published var interstitial = GADInterstitial(adUnitID: "ca-app-pub-3940256099942544/4411468910")
+    @Published var interstitial = GADInterstitial(adUnitID: "ca-app-pub-3940256099942544/4411468910") //test
     @Published var popInterstitial = false
+    @Published var req = GADRequest()
+    @Published var rewardedAdUnit = GADRewardedAd(adUnitID: "ca-app-pub-3940256099942544/1712485313") //test
+    @Published var rewardedAdRequest = GADRequest()
+    @Published var showingUniqueAd = false
+    @Published var rewardFunction:(()->Void)? = nil
 }
